b5f6088b27d7c0d956f46cc94597afa7844eb4fc
Andrea <andrea.doreste@poli.ufrj.br>
2017-06-21 14:34:36 -0300

    heuristica still not working

--- graph_networkx.py
+++ graph_networkx.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 #nodes = ['a','b','c','d','e','f','g','h','i','j','l','k','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 #nodes = ['a','b','c','d','e','f','g','h','i','j','l','k','m','n','o','p','q','r','s','t']
 nodes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21]
-elist = [(1,2,5),(1,4,3),(2,3,9),(1,3,10),(4,5,9),(2,6,7),(6,7,8),(3,5,1),(4,10,5),(7,9,1),(10,12,4),(9,11,1),(15,2,4),(15,6,5),(5,13,2),(14,11,3),(8,12,5),(12,11,2),(16,3,1),(16,7,1),(16,9,1),(16,10,1),(17,13,3),(17,8,9),(18,4,2),(18,2,1),(18,1,3),(18,15,5),(19,6,6),(19,7,7),(20,12,1),(21,13,7)]
+elist = [(1,2,5),(1,4,6),(2,3,9),(1,3,10),(4,5,9),(2,6,7),(6,7,8),(3,5,1),(4,10,5),(7,9,1),(10,12,4),(8,9,2),(9,11,1),(15,2,4),(15,6,5),(5,13,2),(14,11,3),(8,12,5),(12,11,2),(16,3,1),(16,7,1),(16,9,1),(16,10,1),(17,13,3),(17,8,9),(18,4,2),(18,2,1),(18,1,3),(18,15,5),(19,6,6),(19,7,7),(20,12,1),(21,13,7),(10,8,4)]
 G = nx.Graph()
 
 G.add_nodes_from(nodes)
@@ -16,19 +16,19 @@ G.add_weighted_edges_from(elist)
 #print G.edges()
 #print G.nodes()
 '''
-G.add_edge('a','b', weight=5)
-G.add_edge('a','d', weight=6)
-G.add_edge('b','c', weight=9)
-G.add_edge('a','c',weight=10)
-G.add_edge('d','e',weight=9)
-G.add_edge('b','f',weight=7)
-G.add_edge('f','g',weight=8)
-G.add_edge('c','e',weight=1)
-G.add_edge('d','j',weight=5)
-G.add_edge('g','i',weight=1)
-G.add_edge('j','h',weight=4)
-G.add_edge('h','i',weight=2)
-G.add_edge('i','l',weight=1)
+G.add_edge('a','b', weight=5) ok
+G.add_edge('a','d', weight=6) ok
+G.add_edge('b','c', weight=9) ok
+G.add_edge('a','c',weight=10) ok
+G.add_edge('d','e',weight=9) ok
+G.add_edge('b','f',weight=7) ok
+G.add_edge('f','g',weight=8) ok
+G.add_edge('c','e',weight=1) ok
+G.add_edge('d','j',weight=5) ok
+G.add_edge('g','i',weight=1) ok
+G.add_edge('j','h',weight=4) ok
+G.add_edge('h','i',weight=2) ok
+G.add_edge('i','l',weight=1) ok
 
 G.add_edge('o','b',weight=4)
 G.add_edge('o','f',weight=5)
--- heuristica_3.py
+++ heuristica_3.py
@@ -2,22 +2,85 @@ import networkx as nx
 import graph_networkx as gnx
 
 
-def heuristica(k,graph,edge_list):
+
+
+def heuristica(k,graph,edge_list_origin):
+	result = []
+	V=graph.order()	
+	# n = numero de vertices maximo numa particao
+	n = int(round(float(V)/float(k)))
+
 	#Precisa fazer uma copia?
-	edge_list.sort(key=lambda x:x[2])
+	nodes_visited = []
+	
+	edge_list_origin.sort(key=lambda x:x[2])
+	edge_list = list(edge_list_origin)
 	print edge_list
-	i_edge = edge_list.pop(0)
-	print i_edge
-	i_edge_t = (i_edge[0],i_edge[1])
-	print i_edge
-	graph.remove_edge(*i_edge_t)
-	if nx.is_connected(graph):
-		print 'is connected'
-		pass
-	else:
-		graph.add_edge(i_edge[0],i_edge[1],weight=i_edge[2])
+	
+	while (nodes_visited>n):
+	
+		i_edge = edge_list.pop(0)
+		print i_edge
+		i_edge_t = (i_edge[0],i_edge[1])
+		
+		if graph.has_edge(*i_edge_t): 	
+			graph.remove_edge(*i_edge_t)
+		else:
+			continue
+
+		if((i_edge_t[0] in nodes_visited)and (i_edge_t[1] in nodes_visited)):
+			continue
+		elif ((i_edge_t[0] or i_edge_t[1])in nodes_visited) and ((i_edge_t[0] or i_edge_t[1]) not in nodes_visited):
+			print 'FUDEU'
+
+
+		elif (nx.is_connected(graph)):
+			print 'is connected'
+			pass
+		else:
+			connected_components = nx.connected_components(graph)
+			print connected_components
+			size_components = []
+			con_components = []
+			for i in connected_components:
+				con_components.append(i)
+				print i
+				print len(i)
+				size_components.append(len(i))
+
+			if n in size_components:
+				print n
+				print size_components
+				i = size_components.index(n)
+				print 'i: '+str(i)
+				print type(i)
+				#for i in con_components:
+				#	print i
+				result.append(con_components[i])
+				print con_components[i]
+				nodes_visited.extend(con_components[i])
+				for v in con_components[i]:
+					graph.remove_node(v)
+				#reinicia arestas
+				edge_list = list(edge_list_origin)
+				print 'result='
+				print result
+			else:
+				graph.add_edge(i_edge[0],i_edge[1],weight=i_edge[2])	
+
+	print result
+	print graph.nodes()
+
+
+'''
+			graph.add_edge(i_edge[0],i_edge[1],weight=i_edge[2])
+			print 'not connected'
+
+		if ((i_edge_t[0] or i_edge_t[1])in nodes_visited) and ((i_edge_t[0] or i_edge_t[1]) not in nodes_visited):
+			print 'FUDEU'
+'''
 
 G = gnx.G
 edge_list = gnx.elist
 #print edge_list
-heuristica(3,G,edge_list)
+heuristica(4,G,edge_list)
